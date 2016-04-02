353b2a34c609d33ed7f65f88e06783bbe140970f
Seungwoo Yeom <sw.yeom@gmail.com>
2016-04-02 17:22:58 +0900

    Change to modular code

--- routes/ranks.js
+++ routes/ranks.js
@@ -1,121 +1,125 @@
 /**
- * Created by Seungwoo on 2016. 3. 14..
+ * Created by Seungwoo on 2016. 2. 22..
  */
+"use strict";
 
-var cheerio = require('cheerio'),
+let cheerio = require('cheerio'),
     request = require('request');
 
 var rankData = {
     title: String,
     rank: Number,
     status: String,
-    url: String
+    url: String,
+    value: String
+
 };
 
 var rankResult = {
     result: {type: Number, default: 0},
     time: Math.floor(Date.now() / 1000),
-    type: {type: String},
+    type: String,
     data: [rankData]
 };
 
-exports.naver = function (req, response, next) {
-    var url = 'http://www.naver.com';
+let naverParam = {
+    type: 'naver',
+    host: 'http://www.naver.com',
+    selecter: 'ol#realrank a',
+    parserSelecter: function ($, elem) {
+        var data = $(elem);
+        return {
+            title: data.attr('title'),
+            status: data.find('.tx').text(),
+            rank: '',
+            url: data.attr('href'),
+            value: data.find('.rk').text()
+        };
+    }
+};
 
-    request(url, function (err, res, html) {
+let daumParam = {
+    type: 'daum',
+    host: 'http://www.daum.net',
+    selecter: 'ol#realTimeSearchWord > li > div.roll_txt > div:not(.rank_dummy)',
+    parserSelecter: function ($, elem) {
+        var data = $(elem);
+        return {
+            title: realEscape(data.find("span.txt_issue > a").text()),
+            rank: '',
+            status: data.find("span.screen_out").text(),
+            url: data.find("span.txt_issue > a").attr('href'),
+            value: data.find("em.img_vert").text()
+        }
+    }
+};
+
+let nateParam = {
+    type: 'nate',
+    host: 'http://www.nate.com',
+    selecter: 'ol#realTimeSearchWord > li > div.roll_txt > div:not(.rank_dummy)',
+    parserSelecter: function ($, elem) {
+        var data = $(elem);
+        return {
+            title: data.find("p > a").attr('title').text(),
+            rank: '',
+            status: data.find("p > em").text(),
+            url: data.find("p > a").attr('href')
+        }
+    }
+};
+
+let defaultHeader = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'X-Requested-With'
+};
+
+exports.naver = function (req, res) {
+    getRank(naverParam, function (result) {
+        res.set(defaultHeader);
+        res.send(result);
+    });
+};
+
+
+exports.daum = function (req, res) {
+    getRank(daumParam, function (result) {
+        res.set(defaultHeader);
+        res.send(result);
+    });
+};
+
+
+exports.nate = function (req, res) {
+    getRank(nateParam, function (result) {
+        res.set(defaultHeader);
+        res.send(result);
+    });
+};
+
+function getRank(param, handleResult) {
+    let endpoint = param.host;
+    request(endpoint, function (err, res, html) {
         if (!err) {
-            var $ = cheerio.load(html);
+            let $ = cheerio.load(html);
             rankResult.result = 1;
-            rankResult.type = 'naver';
-            $('ol#realrank a').each(function (i, elem) {
-
-                var data = $(this);
-
+            $(param.selecter).each(function (i, elem) {
+                var selectParams = param.parserSelecter($, elem);
+                rankResult.type = param.type;
                 if (i < 10) {
-                    rankResult.data[i] = {
-                        title: data.attr('title'),
-                        rank: i + 1,
-                        status: data.first().find('.tx').text(),
-                        url: data.attr('href')
-                    };
+                    rankResult.data[i] = selectParams;
+                    rankResult.data[i].rank = i + 1;
                 }
-                rankResult.type = 'naver';
-
             });
-
-            response.setHeader('Content-Type', 'application/json');
-            response.send(rankResult);
-
+            handleResult(rankResult);
         } else {
             throw err;
         }
-    })
-};
+    });
+}
 
-exports.daum = function (req, response, next) {
-    var url = 'http://www.daum.net';
 
-    request(url, function (err, res, html) {
-        if (!err) {
-            var $ = cheerio.load(html);
-            rankResult.result = 1;
-            rankResult.type = 'daum';
-            $('ol#realTimeSearchWord > li > div.roll_txt > div:not(.rank_dummy)').each(function (i, elem) {
-
-                var data = $(this);
-
-                if (i < 10) {
-                    rankResult.data[i] = {
-                        title: realEscape(data.find("span.txt_issue > a").text()),
-                        rank: i + 1,
-                        status: data.find("span.screen_out").text(),
-                        url: data.find("span.txt_issue > a").attr('href')
-                    };
-                }
-            });
-
-            response.setHeader('Content-Type', 'application/json');
-            response.send(rankResult);
-
-        } else {
-            throw err;
-        }
-    })
-};
-
-exports.nate = function (req, response, next) {
-    var url = 'http://www.nate.com';
-
-    request(url, function (err, res, html) {
-        if (!err) {
-            var $ = cheerio.load(html);
-            rankResult.result = 1;
-            rankResult.type = 'nate';
-            $('div.area_rtkwd.type_alone div.kwd_list ol li').each(function (i, elem) {
-
-                var data = $(this);
-
-                console.log(data.text());
-
-                if (i < 10) {
-                    rankResult.data[i] = {
-                        title: data.find("p > a").attr('title').text(),
-                        rank: i + 1,
-                        status: data.find("p > em").text(),
-                        url: data.find("p > a").attr('href')
-                    };
-                }
-            });
-
-            response.setHeader('Content-Type', 'application/json');
-            response.send(rankResult);
-
-        } else {
-            throw err;
-        }
-    })
-};
-
-var realEscape = function (target) {
+let realEscape = function (target) {
     return target.replace(/\n/gi, '');
 };
