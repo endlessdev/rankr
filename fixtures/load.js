6c27bfda120e94d603f7db0aa375c102ec3ffcbd
endlessdev <ysw0094@gmail.com>
2017-04-30 21:24:24 +0900

    Remove fixtures

--- fixtures/load.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var fixtures = require('pow-mongodb-fixtures').connect('test');
-var data_1 = require("./data");
-function loadData(cb) {
-    fixtures.clearAndLoad({
-        test: data_1.data
-    }, cb);
-}
-exports.loadData = loadData;
--- fixtures/load.ts
+++
@@ -1,9 +0,0 @@
-let fixtures = require('pow-mongodb-fixtures').connect('test');
-
-import {data} from "./data";
-
-export function loadData(cb: any) {
-    fixtures.clearAndLoad({
-        test: data
-    }, cb);
-}
