f5d743ac07170bf61ebc7c073118dcb1d4e7b3f2
Philip YoonShin <wbvcos@gmail.com>
2017-05-20 14:15:48 -0400

    Update rank.ts

--- routes/rank.ts
+++ routes/rank.ts
@@ -1,45 +1,28 @@
 /// <reference path="../typings/tsd.d.ts"/>
 
-import * as express from "express";
-import {Request, Response} from "express"
-import {CommonHeader} from '../models/CommonHeader';
+import Router from 'koa-router'
+import CommonHeader from '../models/CommonHeader';
 
-import {CommonParser} from "../utils/CommonParser";
-import {NateParser} from "../utils/NateParser";
-import {paramNaver} from "../models/params/NaverParam";
-import {paramDaum} from "../models/params/DaumParam";
+import CommonParser from "../utils/CommonParser";
+import NateParser from "../utils/NateParser";
+import paramNaver from "../models/params/NaverParam";
+import paramDaum from "../models/params/DaumParam";
 
-let router = express();
 let parser = new CommonParser();
 
 /* GET home page. */
-router.get('/', (req: Request, res: Response, next: Function) => {
-    res.render('index', {title: 'Express'})
-});
+router.get('/naver', (ctx, next) => {
+  parser.setParam(paramNaver)
+  ctx.body = await parser.getRank()
+})
 
-router.get('/naver', (req: Request, res: Response, next: Function) => {
-    parser.setParam(paramNaver);
-    parser.getRank(function (result) {
-        res.set(CommonHeader);
-        res.send(result);
-        return result;
-    });
-});
+router.get('/daum', (ctx, next) => {
+  parser.setParam(paramDaum)
+  ctx.body = await parser.getRank()
+})
 
-router.get('/daum', (req: Request, res: Response, next: Function) => {
-    parser.setParam(paramDaum);
-    parser.getRank(function (result) {
-        res.set(CommonHeader);
-        res.send(result);
-        return result;
-    });
-});
-
-router.get('/nate', (req: Request, res: Response, next: Function) => {
-    NateParser.getNateRank(rankResult => {
-        res.set(CommonHeader);
-        res.send(rankResult);
-    })
-});
+router.get('/nate', (ctx, next) => {
+  ctx.body = await Nateparser.getNateRank()
+})
 
 export default router;
