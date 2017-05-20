0aada4bb5bbf8791bd66d90cb26e85af13a59e4e
Philip YoonShin <wbvcos@gmail.com>
2017-05-20 14:19:03 -0400

    Update rank.ts

--- routes/rank.ts
+++ routes/rank.ts
@@ -1,16 +1,18 @@
 /// <reference path="../typings/tsd.d.ts"/>
 
 import Router from 'koa-router'
-import CommonHeader from '../models/CommonHeader';
 
 import CommonParser from "../utils/CommonParser";
 import NateParser from "../utils/NateParser";
 import paramNaver from "../models/params/NaverParam";
 import paramDaum from "../models/params/DaumParam";
-
 let parser = new CommonParser();
 
+const router = new Router({ prefix: '/rank'})
+
+
 /* GET home page. */
+
 router.get('/naver', (ctx, next) => {
   parser.setParam(paramNaver)
   ctx.body = await parser.getRank()
