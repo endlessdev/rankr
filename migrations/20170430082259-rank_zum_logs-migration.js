589e163baa33276fad141bae7d43c3f6b3d79b83
endlessdev <ysw0094@gmail.com>
2017-05-28 15:30:22 +0900

    Write migration, models for zum crawl data

---
+++ migrations/20170430082259-rank_zum_logs-migration.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+     Add altering commands here.
+     Return a promise to correctly handle asynchronicity.
+
+     Example:
+     return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.createTable('rank_zum_logs', {
+      idx: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      rank_crawl_idx: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "rank_crawl_logs",
+          key: "idx"
+        },
+        onDelete: "CASCADE"
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      rank: {
+        type: Sequelize.INTEGER
+      }
+    }, {
+      timestamps: false,
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+     Add reverting commands here.
+     Return a promise to correctly handle asynchronicity.
+
+     Example:
+     return queryInterface.dropTable('users');
+     */
+    return queryInterface.dropTable('rank_zum_logs');
+  }
+};
