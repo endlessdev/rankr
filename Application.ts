a2b51ee95786bb711b7111936bb9a82208f2b14d
endlessdev <ysw0094@gmail.com>
2017-05-29 23:59:53 +0900

    Add recent analytics api

--- Application.ts
+++ Application.ts
@@ -4,10 +4,12 @@ import * as Koa from 'koa'
 import * as bodyParser from 'koa-bodyparser'
 import * as logger from 'koa-logger'
 import * as serve from 'koa-static'
+import * as cors from 'koa-cors'
 
 import * as path from 'path'
 
 import rank from './routes/rank';
+import api from './routes/api';
 import {crawlJob} from './database/crawler'
 
 const app = new Koa();
@@ -18,7 +20,9 @@ const bpOption = {extendTypes: {json: ['application/x-javascript']}};
 app
     .use(logger())
     .use(bodyParser(bpOption))
+    .use(cors())
     .use(rank.routes())
+    .use(api.routes())
     .use(serve(dist));
 
 crawlJob.start();
---
+++ routes/api.ts
@@ -0,0 +1,47 @@
+/// <reference path="../typings/tsd.d.ts"/>
+
+import * as Router from 'koa-router'
+import {sequelize, Sequelize} from "../database/index"
+
+import async = Q.async;
+
+const router = new Router({prefix: '/analytics'});
+
+/* GET home page. */
+
+router.get('/recent', async (ctx, next) => {
+
+    const RAW_QUERY = `
+   SELECT title,
+        Avg(rank)    AS rank_avg,
+        Count(title) AS count
+    FROM   rank_naver_logs
+    UNION
+    SELECT title,
+        Avg(rank)    AS rank_avg,
+        Count(title) AS count
+    FROM   rank_daum_logs
+    UNION
+    SELECT title,
+        Avg(rank)    AS rank_avg,
+        Count(title) AS count
+    FROM   rank_zum_logs
+    WHERE  ( rank_crawl_idx IN (SELECT idx
+    FROM   rank_crawl_logs
+    WHERE  createdat >= Now() - INTERVAL 1 hour) )
+    GROUP  BY title
+    ORDER  BY rank_avg ASC
+    LIMIT
+    10`;
+
+    let result = [];
+    await sequelize.query(RAW_QUERY).spread(async (results, metadata) => {
+        result = results;
+    });
+
+    ctx.body = result;
+
+});
+
+
+export default router;
