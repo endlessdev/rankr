6923cb8430c68286869d71b90520ca79e414e536
endlessdev <ysw0094@gmail.com>
2017-05-30 17:41:49 +0900

    Add today analytics api and modify query of recent api

--- routes/api.ts
+++ routes/api.ts
@@ -26,13 +26,65 @@ router.get('/recent', async (ctx, next) => {
         Avg(rank)    AS rank_avg,
         Count(title) AS count
     FROM   rank_zum_logs
-    WHERE  ( rank_crawl_idx IN (SELECT idx
+    WHERE  ( rank_crawl_idx IN (SELECT idx           
     FROM   rank_crawl_logs
     WHERE  createdAt >= Now() - INTERVAL 1 hour) )
     GROUP  BY title
     ORDER  BY rank_avg ASC
     LIMIT
     10`;
+
+    let result = [];
+    await sequelize.query(RAW_QUERY).spread(async (results, metadata) => {
+        result = results;
+    });
+
+    ctx.body = result;
+
+});
+
+router.get('/today', async (ctx, next) => {
+
+    const RAW_QUERY = `
+   SELECT
+  title,
+  Avg(rank)    AS rank_avg,
+  Count(title) AS count,
+  rank_crawl_logs.*
+FROM rank_naver_logs
+  LEFT JOIN rank_crawl_logs ON rank_crawl_idx = rank_crawl_logs.idx
+  WHERE ((rank_crawl_idx IN (SELECT idx
+                           FROM rank_crawl_logs
+                           WHERE createdAt >= Now() - INTERVAL 1 DAY))) AND
+      (createdAt >= Now() - INTERVAL 1 DAY)
+UNION
+SELECT
+  title,
+  Avg(rank)    AS rank_avg,
+  Count(title) AS count,
+  rank_crawl_logs.*
+FROM rank_daum_logs
+  LEFT JOIN rank_crawl_logs ON rank_crawl_idx = rank_crawl_logs.idx
+  WHERE ((rank_crawl_idx IN (SELECT idx
+                           FROM rank_crawl_logs
+                           WHERE createdAt >= Now() - INTERVAL 1 DAY))) AND
+      (createdAt >= Now() - INTERVAL 1 DAY)
+UNION
+SELECT
+  title,
+  Avg(rank)    AS rank_avg,
+  Count(title) AS count,
+  rank_crawl_logs.*
+FROM rank_zum_logs
+  LEFT JOIN rank_crawl_logs ON rank_crawl_idx = rank_crawl_logs.idx
+
+WHERE ((rank_crawl_idx IN (SELECT idx
+                           FROM rank_crawl_logs
+                           WHERE createdAt >= Now() - INTERVAL 1 DAY))) AND
+      (createdAt >= Now() - INTERVAL 1 DAY)
+GROUP BY title
+ORDER BY createdAt DESC,
+  count DESC`;
 
     let result = [];
     await sequelize.query(RAW_QUERY).spread(async (results, metadata) => {
