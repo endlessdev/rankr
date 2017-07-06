59cfa7d492f7109e89ab8fd7860c0430f262a4fa
endlessdev <ysw0094@gmail.com>
2017-07-06 15:10:10 +0900

    Change parser params for changed daum html structure

--- models/params/daum-param.ts
+++ models/params/daum-param.ts
@@ -3,14 +3,15 @@ import { ParserParam } from '../parser-param';
 const parserParam: ParserParam = {
   type: 'daum',
   url: 'http://www.daum.net',
-  querySelector: 'ol.list_hotissue > li .rank_cont:not([aria-hidden])',
+  querySelector:
+    '.hotissue_layer .list_hotissue.issue_row:not(.list_mini) > li .rank_cont[aria-hidden=true]',
   parserSelector: ($, elem) => {
     const data = $(elem);
     return {
-      title: data.find('.txt_issue > a').attr('title'),
+      title: data.find('.txt_issue > a.link_issue:first-child').text(),
       rank: '',
-      status: data.find('em.rank_result .ir_wa').text(),
-      value: data.find('em.rank_result').text().replace(/[^0-9]/g, ''),
+      status: data.find('.rank_result .ico_pctop .ir_wa').text(),
+      value: data.find('.rank_result').text().replace(/[^0-9]/g, ''),
     };
   },
 };
