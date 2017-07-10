47f553757f579742484540105547f7c2697ae68b
endlessdev <ysw0094@gmail.com>
2017-07-11 00:47:45 +0900

    Separate news parser

--- routes/news.ts
+++ routes/news.ts
@@ -1,42 +1,17 @@
 /// <reference path="../typings/tsd.d.ts"/>
 
 import * as Router from 'koa-router';
-import * as request from 'request-promise';
-import * as xml2js from 'xml2js';
-import * as moment from 'moment';
 import * as timezone from 'moment-timezone';
 
 import async = Q.async;
+import newsParser from '../utils/news-parser';
 
-const router = new Router({ prefix: '/v1/news' });
+const router = new Router({ prefix: '/news' });
 
 timezone().tz('Asia/Seoul').format();
 
 router.get('/:keyword', async (ctx) => {
-  const requestURL =
-    `http://newssearch.naver.com/search.naver?where=rss&query=${encodeURI(ctx.params.keyword)}`;
-  const rssResponse = await request(requestURL);
-  await xml2js.parseString(rssResponse, async (err, result) => {
-    const newsData = result.rss.channel[0].item;
-
-    await newsData.forEach((news, index, arr) => {
-
-      for (const element in newsData[index]) {
-        if (newsData[index].hasOwnProperty(element)) {
-          newsData[index][element] = newsData[index][element][0];
-        }
-      }
-
-      if (news['media:thumbnail']) {
-        newsData[index].thumb = news['media:thumbnail'].$.url.replace(/thumb140/gi, 'thumb');
-        delete newsData[index]['media:thumbnail'];
-      }
-
-      newsData[index].pubDate = moment(newsData[index].pubDate).fromNow();
-
-    });
-    ctx.body = newsData;
-  });
+  ctx.body = newsParser.getNewsDataByKeyword(ctx.params.keyword);
 });
 
 export default router;
---
+++ utils/news-parser.ts
@@ -0,0 +1,27 @@
+import * as request from 'request-promise';
+import * as xml2js from 'xml2js';
+import * as moment from 'moment';
+
+export default class NewsParser {
+
+  static getNewsDataByKeyword = async (searchKeyword: string) => {
+    console.log(searchKeyword);
+    let newsData: any = [];
+    const rssURL = `http://newssearch.naver.com/search.naver?where=rss&query=`;
+    const requestURL =
+      `${rssURL}}${encodeURI(searchKeyword)}`;
+    const rssResponse = await request(requestURL);
+    await xml2js.parseString(rssResponse, async (err, result) => {
+      newsData = result.rss.channel[0].item;
+      await newsData.forEach(async (news, index) => {
+        for (const element in newsData[index]) {
+          if (newsData[index].hasOwnProperty(element)) {
+            newsData[index][element] = newsData[index][element][0];
+          }
+        }
+        newsData[index].pubDate = moment(newsData[index].pubDate).fromNow();
+      });
+    });
+    return newsData;
+  }
+}
