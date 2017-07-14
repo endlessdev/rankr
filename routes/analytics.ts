24c86503395a798c798b9a6027709a1bddbbfd8d
endlessdev <ysw0094@gmail.com>
2017-07-14 11:04:12 +0900

    Fix raw query error

--- routes/analytics.ts
+++ routes/analytics.ts
@@ -117,7 +117,7 @@ router.get('/keyword/:keyword', async (ctx, next) => {
   FROM rank_crawl_logs 
   LEFT JOIN rank_${rank}_logs ON rank_crawl_logs.idx = rank_${rank}_logs.rank_crawl_idx
   WHERE createdAt >= Now() - INTERVAL 1 HOUR AND
-      REPLACE(title, ' ', '') = REPLACE('${searchKeyword}', ' ', '')
+      REPLACE(title, ' ', '') = REPLACE(${searchKeyword}, ' ', '')
   GROUP BY createdAt
   ORDER BY createdAt ASC;`;
 
