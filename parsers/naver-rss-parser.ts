10bf63f55472d8a11b8b7501f77405cd3b6207d9
Seungwoo Yeom <ysw0094@gmail.com>
2018-04-25 01:39:13 +0900

    Separate RSS Parser as a class

---
+++ parsers/naver-rss-parser.ts
@@ -0,0 +1,29 @@
+import * as request from 'request-promise';
+import * as xml2js from 'xml2js';
+import * as moment from 'moment';
+
+export default class NaverRSSParser {
+  public static getNewsDataByKeyword = async (searchKeyword: string) => {
+    let newsData: any = [];
+    const rssURL = `http://newssearch.naver.com/search.naver?where=rss&query=`;
+    const requestURL =
+      `${rssURL}}${encodeURI(searchKeyword)}`;
+    const rssResponse = await request(requestURL);
+    await xml2js.parseString(rssResponse, async (err, result) => {
+      newsData = result.rss.channel[0].item;
+      await newsData.forEach(async (news, index) => {
+        for (const element in newsData[index]) {
+          if (newsData[index].hasOwnProperty(element)) {
+            newsData[index][element] = newsData[index][element][0];
+          }
+        }
+        if (news['media:thumbnail']) {
+          newsData[index].thumb = news['media:thumbnail'].$.url.replace(/thumb140/gi, 'thumb');
+          delete newsData[index]['media:thumbnail'];
+        }
+        newsData[index].pubDate = moment(newsData[index].pubDate).fromNow();
+      });
+    });
+    return newsData;
+  }
+}
--- routes/news.ts
+++ routes/news.ts
@@ -2,16 +2,14 @@
 
 import * as Router from 'koa-router';
 import * as timezone from 'moment-timezone';
-
-import async = Q.async;
-import naverParser from '../parsers/naver-parser';
+import naverRssParser from '../parsers/naver-rss-parser';
 
 const router = new Router({ prefix: '/v1/news' });
 
 timezone().tz('Asia/Seoul').format();
 
 router.get('/:keyword', async (ctx) => {
-  ctx.body = await naverParser.getNewsDataByKeyword(ctx.params.keyword);
+  ctx.body = await naverRssParser.getNewsDataByKeyword(ctx.params.keyword);
 });
 
 export default router;
